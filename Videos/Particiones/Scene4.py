4867d30d7bc1e5c716803b1da9d24fbffee0b201
Jocsan Ely <jocsanelyrueda@gmail.com>
2023-12-11 16:59:40 -0600

    Escena 4

--- Videos/Particiones/Scene4.py
+++ Videos/Particiones/Scene4.py
@@ -7,7 +7,7 @@ class Scen4(Scene):
         number_plane = NumberPlane(
             background_line_style={
                 "stroke_color": BLUE,
-                "stroke_width": 1,
+                "stroke_width": 2,
                 "stroke_opacity": 0.4,
             }
         )
@@ -16,7 +16,25 @@ class Scen4(Scene):
             lambda t: np.cos(t),
             color=BLUE,
         )
+
         form_cos = MathTex("f(x)= cos(x)").next_to(cos_func, UP).shift([-2, 0, 0])
+        tan_func = FunctionGraph(lambda t: np.tan(t), color=TEAL)
+        form_tan = MathTex("f(x)=tan(x)")
+        form_tan.move_to(form_cos.get_center()).shift([-1.5, 0, 0])
+
+        lineal_func = FunctionGraph(lambda t: t, color=GREEN)
+        form_lineal = MathTex("f(x)=x").move_to(form_cos.get_center())
+
+        form_partiton = MathTex("p(n)=?").move_to(form_cos.get_center())
+
         self.add(number_plane)
         self.play(Create(cos_func), Write(form_cos))
+        self.wait()
+        self.play(Transform(form_cos, form_tan), Transform(cos_func, tan_func))
+        self.wait()
+        self.play(Transform(form_cos, form_lineal), Transform(cos_func, lineal_func))
+        self.wait()
+        self.play(Uncreate(form_cos), Uncreate(cos_func))
+        self.play(Write(form_partiton))
+
         self.wait()
